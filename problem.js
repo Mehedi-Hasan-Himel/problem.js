1204013e2e085beb5476f112d111c3c1d1b83ada
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-02-22 21:05:57 +0600

    problem-02

--- problem.js
+++ problem.js
@@ -10,19 +10,21 @@ console.log(breakfastPrice);
 
 // Problem: 01 End;
 
-
 // Problem: 02 Start;
 /*
+// Problem solving for showing messages in the morning, afternoon, evening and night according to the time.
+
 let time = 4;
-if (time >= 5 && time < 12) {
+if (time >= 4 && time < 12) {
   console.log(`Good morning.`);
 } else if (time >= 12 && time <= 14) {
   console.log(`Good noon.`);
-} else if (time >= 15 && time <= 19) {
+} else if (time >= 15 && time <= 17) {
+  console.log(`Good afternoon.`);
+}else if (time >= 18 && time <= 19) {
   console.log(`Good evening.`);
 } else {
   console.log(`Good night`);
 }
 */
 // Problem: 02 End;
-
