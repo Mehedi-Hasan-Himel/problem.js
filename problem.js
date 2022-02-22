f411ca3b1038f8a30eedb5bafadadad4c0beb9f4
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-02-22 20:50:29 +0600

    first commit

---
+++ problem.js
@@ -0,0 +1,28 @@
+// Problem: 01 Start;
+/*
+let bananaPrice = 5;
+let breadPrice = 10;
+
+let breakfastPrice = bananaPrice + breadPrice;
+
+console.log(breakfastPrice);
+*/
+
+// Problem: 01 End;
+
+
+// Problem: 02 Start;
+/*
+let time = 4;
+if (time >= 5 && time < 12) {
+  console.log(`Good morning.`);
+} else if (time >= 12 && time <= 14) {
+  console.log(`Good noon.`);
+} else if (time >= 15 && time <= 19) {
+  console.log(`Good evening.`);
+} else {
+  console.log(`Good night`);
+}
+*/
+// Problem: 02 End;
+
