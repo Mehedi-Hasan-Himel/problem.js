5b1a38e29ea9e1672d4aa0bee73439130d0e4a1f
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-03-08 19:29:38 +0600

    problem 12

--- problem.js
+++ problem.js
@@ -73,3 +73,131 @@ while (i >= 1) {
 }
 // Problem: 04 End;
 */
+
+// Problem: 05 Start;
+
+// coffeePrice 100 টাকার কম হলে coffee খাবো না হলে চা খাবো ।
+
+/*
+
+let coffeePrice = 105;
+
+if (coffeePrice < 100) {
+  console.log(`I'll drink a cup of Coffee.`);
+} else {
+  console.log(`I have to drink a cup of Tea`);
+}
+
+*/
+// Problem: 05 End;
+
+// Problem: 06 Start;
+
+// পকেটে 5000 টাকার বেশি বা সমান থাকলে Cox's Bazar যাবো আর না থাকলে আশুলিয়া যাবো ।
+
+/*
+
+let pocketMoney = 6700;
+
+if (pocketMoney >= 5000) {
+  console.log(`I'll visit "Cox's Bazar"`);
+} else {
+  console.log(`I'll visit Asulia`);
+}
+
+*/
+// Problem: 06 End;
+
+// Problem: 07 Start;
+
+// iceCreamPrice 50 টাকার কম হলে এবং গরম লাগলে (isHot) ice-Cream খাবো , নচেৎ filtered water খাবো ।
+
+/*
+
+let iceCreamPrice = 40;
+isHot = false;
+
+if ((iceCreamPrice < 50) && (isHot = true)) {
+  console.log(`I'll buy a "ice Cream"`);
+} else {
+  console.log(`I've to drink filtered water.`);
+}
+
+*/
+// Problem: 07 End;
+
+// Problem: 08 Start;
+
+// পরিবারের সদস্যদের নাম নিয়ে একটা array declear করো ।
+/*
+
+let family = [`Mustafa Kamal`, `Latifa Helen`,`Mithel Hasan Hira`, `Mehedi Hasan Himel`, `Sabed Ali`, `Ayesha` ];
+
+console.log(family);
+*/
+
+// Problem: 08 End;
+
+// Problem: 09 Start;
+
+// পরিবারের সদস্য সংখ্যা কতো ? array এর 2nd position এ কে আছে ?
+/*
+let family = [
+  `Mustafa Kamal`,
+  `Latifa Helen`,
+  `Mithel Hasan Hira`,
+  `Mehedi Hasan Himel`,
+  `Sabed Ali`,
+  `Ayesha`,
+];
+
+let memberOfFamily = family.length;
+console.log( `Member of family ` + memberOfFamily);
+
+let position2nd = family[2];
+console.log( `2nd position of this array ` + position2nd);
+*/
+// Problem: 09 End;
+
+// Problem: 10 Start;
+
+// কলি কে বিয়ে করেছি , এখন কলি আমার পরিবারের সদস্য তাই তার নাম family array তে যোগ করতে হবে ।
+/* 
+let family = [
+  `Mustafa Kamal`,
+  `Latifa Helen`,
+  `Mithel Hasan Hira`,
+  `Mehedi Hasan Himel`,
+  `Sabed Ali`,
+  `Ayesha`,
+];
+
+let newMember = family.push(`Koli`) 
+console.log(family);
+*/
+// Problem: 10 End;
+
+// Problem: 11 Start;
+
+// একশবার লিখতে হবে " আমি কলম দিয়ে কাউকে খোঁচা দিবো না " এই লাইন 100  বার print হওয়ার program , while loop দিয়ে লিখতে হবে ।
+/*
+let poke = 0;
+
+while (poke <100) {
+  console.log(`" আমি কলম দিয়ে কাউকে খোঁচা দিবো না "`);
+  poke++; 
+}
+*/
+// Problem: 11 End;
+
+// Problem: 12 Start;
+
+// " ঈদ মোবারক " কথা টা 14 জনকে SMS করে পাঠাতে হবে সেই program:
+/*
+let sms = 0
+while (sms <14) {
+  console.log(`" ঈদ মোবারক "`);
+  sms++;
+}
+*/
+// Problem: 12 End;
