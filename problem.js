659fbefef033bb54656302bb9714f6a559d17120
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-05 17:49:02 +0600

    31

--- problem.js
+++ problem.js
@@ -519,4 +519,22 @@ switch (!!mark) {
 }
 // Problem: 30 End;
 
+
+// Problem: 31 Start;
+// তিনটি সংখ্যার মধ্যে বড় সংখ্যা নির্ণয় করো ।
+
+const business = 450;
+const minister = 350;
+const army = 600;
+
+if (business > minister && business > army) {
+  console.log(`Business is bigger.`);
+} else if (minister > business && minister > army) {
+  console.log(`Minister is bigger.`);
+} else {
+  console.log(`Army is bigger.`);
+}
+
+// Problem: 31 End;
+
 */
