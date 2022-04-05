377826c06cde8956556ab9e8f27cc873510c12b1
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-05 22:08:34 +0600

    33

--- problem.js
+++ problem.js
@@ -553,12 +553,27 @@ function biggestNumber(number1, number2, number3) {
 }
 // Problem: 32 End;
 
-*/
 
 
 // Problem: 33 Start;
+
 // write a function to find the smallest number from three number.
+
+
+function smallestNumber(number1, number2, number3) {
+  if (number1 < number2 && number1 < number3) {
+    return number1;
+  } else if (number2 < number1 && number2 < number3) {
+    return number2;
+  } else {
+    return number3;
+  }
+}
+
+let small = smallestNumber(11, 3, 7);
+console.log(small);
 
 
 // Problem: 33 End;
 
+*/
